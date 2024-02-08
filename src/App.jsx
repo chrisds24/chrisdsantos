f7e1bfe3bdc7b409db8b4b7fbc729caf0b2f9c66
Christian Delos Santos <christiandelossantos@Christians-MacBook-Air.local>
2024-02-08 02:13:19 -0800

    added app bar, working on main content

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
+import { Box } from '@mui/material';
+import MyAppBar from './components/MyAppBar';
+import Content from './components/Content';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{display: 'flex'}}>
+      <MyAppBar />
+      <Content />
+    </Box>
   );
 }
 
