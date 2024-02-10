5c5f8a5f94d705accd19668aaee1530792fa6aef
Christian Delos Santos <christiandelossantos@Christians-MacBook-Air.local>
2024-02-10 06:46:24 -0800

    removed unused imports

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 
 import { Box } from '@mui/material';
-import { Typography } from '@mui/material';
-import {List, ListItem, ListItemIcon, ListItemText} from '@mui/material';
-import BugReportIcon from '@mui/icons-material/BugReport';
-import { Link } from '@mui/material';
-import DownloadIcon from '@mui/icons-material/Download';
-import FileOpenOutlinedIcon from '@mui/icons-material/FileOpenOutlined';
-import { Button } from '@mui/material';
 
 import AboutMe from './AboutMe';
 import Resume from './Resume';
