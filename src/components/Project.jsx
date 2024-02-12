caa4fc65a4123bab9bb2a6dd1750a50e278449a3
Christian Delos Santos <christiandelossantos@Christians-MacBook-Air.local>
2024-02-11 22:45:00 -0800

    done with project component?

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 import { Paper } from '@mui/material';
 import { Box } from '@mui/material';
 import {List, ListItem, ListItemIcon, ListItemText} from '@mui/material';
-import WaterDropIcon from '@mui/icons-material/WaterDrop';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import { Typography } from '@mui/material';
+import CodeIcon from '@mui/icons-material/Code';
+import { Chip } from '@mui/material';
+import { Grid } from '@mui/material';
 
 function Project(props) {
   return (
     <Paper
-      sx={{backgroundColor: '#3b9dff', marginBottom: '7vh', width: '90vw'}}
+      sx={{backgroundColor: '#3b9dff', marginBottom: '7vh', width: '90vw', padding: '2vh 1vw 2vh 1vw'}}
       elevation={6}
     >
       <Box
@@ -20,7 +22,7 @@ function Project(props) {
           flexDirection: {xs: 'column' , md: 'row'}
         }}
       >
-        <img src={'ngrx.svg'} alt={'NgRx'} style={{ height: '33vh', objectFit:'contain'}} />
+        <img src={props.imgSource} alt={'NgRx'} style={{ height: '40vh', objectFit:'contain'}} />
         <Card
           elevation={0}
         >
@@ -28,20 +30,29 @@ function Project(props) {
             sx={{
               display: 'flex',
               flexDirection: 'column',
-              backgroundColor: '#3b9dff'
+              backgroundColor: '#3b9dff',
+              padding: {md: '0vh 0vw 0vh 0vw'},
+              '&:last-child': {paddingBottom: 0}
             }}
           > 
-            <Typography variant="string" gutterBottom color='#00274e' fontWeight='bold' sx={{textDecoration: 'underline', fontSize: '22px'}}>
+            <Typography variant="string" color='#00274e' fontWeight='bold' sx={{textDecoration: 'underline', fontSize: '22px'}} textAlign={'center'}>
               {props.name}
             </Typography>
-            <Typography variant="string" gutterBottom color='#00274e' fontWeight='bold' sx={{textDecoration: 'underline', fontSize: '22px'}}>
+            <Typography variant="string" color='#00274e' fontWeight='bold' sx={{fontSize: '20px'}} textAlign={'center'}>
               {props.description}
             </Typography>
-            <List>
+            <List
+              sx={{
+                padding: '0',
+                margin: '0'
+              }}
+            >
               {props.notes.map((note) => (
-                <ListItem>
+                <ListItem
+                  sx={{padding: '0.6vh 2vw'}}
+                >
                   <ListItemIcon>
-                    <WaterDropIcon sx={{ color: '#ffffff' }} />
+                    <CodeIcon sx={{ color: '#ffffff' }} />
                   </ListItemIcon>
                   <ListItemText
                     primary={`${note}`}
@@ -54,6 +65,13 @@ function Project(props) {
                 </ListItem>
               ))}
             </List>
+            <Grid rowSpacing={1} columnSpacing={1} container direction='row' sx={{paddingTop: '12px'}}>
+              {props.stack.map((tech, idx) => (
+                <Grid item key={idx}>
+                  <Chip label={tech} sx={{backgroundColor: '#00274e', color: '#ffffff'}} />
+                </Grid>
+              ))}
+            </Grid>
           </CardContent>
         </Card>
       </Box>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,13 +6,15 @@ import { Typography } from '@mui/material';
 import Project from './Project';
 
 const proj1Notes = [
+  'Quibusdam temporibus beatae doloremque voluptatum doloribus soluta accusamus porro reprehenderit eos inventore facere, fugit, molestiae ab officiis illo voluptates recusandae.',
   'hello',
   'world',
   'i need a job, seriously',
   'how do these look like in the box?',
-  'askdhaskdhaskjdhaskjdhakdhaksjdhaskdhafcdhjsvgcyeafjahdsbcajhdvbakwdvagdgakgakjhsdaskjdhakjsdgajshdgajhsd',
-  'Quibusdam temporibus beatae doloremque voluptatum doloribus soluta accusamus porro reprehenderit eos inventore facere, fugit, molestiae ab officiis illo voluptates recusandae.'
+  'askdhaskdhaskjdhaskjdhakdhaksjdhaskdhafcdhjsvgcyeafjahdsbcajhdvbakwdvagdgakgakjhsdaskjdhakjsdgajshdgajhsd'
 ];
+
+const proj1Stack = ['TypeScript', 'React', 'Next.js', 'Tailwind CSS', 'TypeGraphQL', 'Node.js', 'Express', 'PostgreSQL'];
 
 function Projects() {
   return (
@@ -27,11 +29,11 @@ function Projects() {
         alignItems: 'center'
       }}
     >
-      <Typography variant="h4" gutterBottom color='#3b9dff' fontWeight='bold' sx={{textDecoration: 'underline'}}>
-        Resume
+      <Typography variant="h4" gutterBottom color='#3b9dff' fontWeight='bold' sx={{textDecoration: 'underline', paddingBottom: '4vh'}}>
+        Projects
       </Typography>
       <Stack spacing={2}>
-        <Project imgSource={'ngrx.svg'} name={'SlugSell'} description={'SlugSell description'} notes={proj1Notes} />
+        <Project imgSource={'render.svg'} name={'SlugSell'} description={'SlugSell description'} notes={proj1Notes} stack={proj1Stack} />
       </Stack>
     </Box>
   );
